94a30aa911f6c3ed5bc01e9b36a930233f249079
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-05-31 16:09:09 +0900

    Update top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        My portfolio
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div
+    class="min-h-screen flex justify-center items-center text-center bg-gray-600"
+  >
+    <div class="flex justify-center">
+      <div class="w-1/3 mx-10 relative">
+        <p class="absolute text-3xl z-10">Profile</p>
+        <nuxt-link to="profile">
+          <img class="blur-img" src="~/assets/img/profile.png" alt="works" />
+        </nuxt-link>
+      </div>
+      <div class="w-1/3 mx-10 relative">
+        <p class="absolute text-3xl z-10">Works</p>
+        <nuxt-link to="works">
+          <img class="blur-img" src="~/assets/img/works.png" alt="works" />
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -26,43 +22,19 @@
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  layout() {
+    return 'headerNone';
+  }
+});
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.blur-img {
+  filter: blur(0.5rem);
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.blur-img:hover {
+  filter: blur(0.04rem);
 }
 </style>
